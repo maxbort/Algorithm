a9419f1f3736f88e49b174510b082b0aa8615c20
maxbort <cjfgh2449@naver.com>
2023-02-19 23:41:41 +0900

    효율적인 화폐구성 코드 수정

--- 알고리즘 스터디/DP/이코테_효율적인화폐구성_DP.py
+++ 알고리즘 스터디/DP/이코테_효율적인화폐구성_DP.py
@@ -12,8 +12,10 @@ d[0] = 0
 
 for coin in coins:
     for i in range(coin,m+1):
-        if d[i - coin] <= INF:
+        if d[i - coin] < INF:
             d[i] = min(d[i],d[i-coin]+1)
 
-print(d)
-print(d[m])
+if d[m] == INF:
+    print(-1)
+else:
+    print(d[m])
