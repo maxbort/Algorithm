67baf730d34d5cf79afa20c133922b5df54cab42
maxbort <cjfgh2449@naver.com>
2023-02-19 23:35:25 +0900

    바닥공사 코드 수정

--- 알고리즘 스터디/DP/이코테_바닥공사_DP.py
+++ 알고리즘 스터디/DP/이코테_바닥공사_DP.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 n = int(input())
 
-d = [0] * 10
+d = [0] * 1001
 d[0] = 0
 d[1] = 1
 d[2] = 3
@@ -11,5 +11,4 @@ d[2] = 3
 for i in range(3,n+1):
     d[i] = (d[i-2]*2 + d[i-1]) % 796796
 
-print(d)
 print(d[n])
