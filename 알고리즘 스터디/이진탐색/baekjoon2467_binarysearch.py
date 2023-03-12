eb5e41e2f316b73c13a92a68684660897996f643
CHOI HyoJun <cjfgh2449@gmail.com>
2023-03-12 14:47:24 +0900

    2023-03-12 알고리즘 문제 풀이

---
+++ 알고리즘 스터디/이진탐색/baekjoon2467_binarysearch.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+liq = list(map(int,input().split()))
+
+answer = 1000000000
+ans1 = 0
+ans2 = 0
+
+for i in range(n-1):
+    check = liq[i]
+    
+    start = i+1
+    end = n-1
+    
+    while start <= end:
+        mid = (start+end)//2
+        temp = check + liq[mid]
+
+        if abs(temp) < answer:
+            answer = abs(temp)
+            ans1 = i
+            ans2 = mid
+            
+            if temp == 0:
+                break
+            
+        if temp < 0:
+            start = mid + 1
+        else:
+            end = mid - 1
+            
+print(liq[ans1], liq[ans2])
---
+++ 알고리즘 스터디/이진탐색/baekjoon2467_twoporinter.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+liq = list(map(int,input().split()))
+
+start = 0
+end = n - 1
+
+temp = abs(liq[start] + liq[end])
+ans1 = 0
+ans2 = n-1
+while start < end:
+    a = liq[start] + liq[end]
+    if abs(a) < temp:
+        temp = a
+        ans1 = start
+        ans2 = end
+        if a == 0:
+            break
+        
+    if a < 0:
+        start += 1
+    else:
+        end -= 1
+print(liq[ans1], liq[ans2])
